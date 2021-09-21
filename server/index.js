655b557be6d0a77bdd47a49d3ab37d0b3e080d55
EugeneFaceControl <eugenefacecontrol@gmail.com>
2021-09-21 23:09:58 +0300

    almost done project

---
+++ server/index.js
@@ -0,0 +1,90 @@
+let keys = require("./keys");
+
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// Added:
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Was:
+// app.use(bodyParser.json());
+
+// Postgress Client Setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+// Was:
+// pgClient.on('error', () => console.log('Lost PG connection'));
+
+// pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+// .catch((err) => console.log(err));
+
+// Now:
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS value (number INT)")
+    .catch((err) => console.error(err));
+});
+
+// Redis Cliend Setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+//  Postgress
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * FROM values");
+  res.send(values.rows);
+});
+
+// Redis
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+      return res.status(422).send('index too high');
+  }
+
+  // Replace the Nothing yet! with new value (index)
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+}); 
